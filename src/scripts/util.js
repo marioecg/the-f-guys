5cde050a9eeb9b7a5cb7f2373a3e4b65780581bf
marioecg <marioecg@gmail.com>
2021-01-28 22:34:51 -0700

    bounding box center hack

--- src/scripts/util.js
+++ src/scripts/util.js
@@ -4,19 +4,28 @@ import { GLText } from './gl/GLText';
 // Create and arrange 3D text 
 export function create3DText(ctx) {
   // Create class instance
-  const text1 = new GLText(ctx, { text: 'THE' });
-  const text2 = new GLText(ctx, { text: 'F' });
-  const text3 = new GLText(ctx, { text: 'GUYS' });
+  const text1 = new GLText(ctx, { text: 'THE', color: [1.0, 1.0, 1.0] });
+  const text2 = new GLText(ctx, { text: 'F', color: [0.0, 1.0, 0.4588]});
+  const text3 = new GLText(ctx, { text: 'GUYS', color: [1.0, 1.0, 1.0] });
 
   // Load and create text geometry
   text1.loadText();
   text2.loadText();
   text3.loadText();
 
+  // Calculate bounding box to arrange text
+  text1.mesh.geometry.computeBoundingBox();
+  text2.mesh.geometry.computeBoundingBox();
+  text3.mesh.geometry.computeBoundingBox();
+
+  const text1width = text1.mesh.geometry.bounds.max.x - text1.mesh.geometry.bounds.min.x;
+  const text1height = text1.mesh.geometry.bounds.max.y - text1.mesh.geometry.bounds.min.y;
+  const text3width = text3.mesh.geometry.bounds.max.x - text3.mesh.geometry.bounds.min.x;    
+  const text3height = text3.mesh.geometry.bounds.max.y - text3.mesh.geometry.bounds.min.y;    
+
   // Position words
-  text1.position.set(-2, 0, 0);
-  text2.position.set(0.05, 0, 0);
-  text3.position.set(-2, -0.75, 0);
+  text2.position.set(text1width + 0.2, 0.0, 0.0);
+  text3.position.set(0.0, -text1height * 1.1, 0.0);    
 
   // Group texts in a container
   const group = new Transform();
@@ -24,7 +33,7 @@ export function create3DText(ctx) {
   group.addChild(text2);
   group.addChild(text3);
 
-  console.log(group);
+  group.position.set(-text3width / 2, (text3height / 2) * 1.1, 0);
 
   return group;
 }
